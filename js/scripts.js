a3fce451b9fa820024ab46144a394bfe51524e26
GabCB <gabycuencas@gmail.com>
2022-12-28 22:40:03 +0100

    Update scripts.js
    create varieble pokemonList
    assign keys id, name, type, height, ability to variable
    assign values to keys

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-alert('Hello World');
-let favoriteFood = 'Crackers';
-document.write(favoriteFood);
+let pokemonList = [
+    {ID:"1", name: "Bulbasaur", type: ["grass", "poison"], height: 2.33, ability:"overgrow"},
+    {ID:"2", name: "Ivysaur", type: ["grass", "poison"], height: 3.25, ability:"overgrow"},
+    {ID:"3", name: "Venusaur", type: ["grass", "poison"], height: 6.58, ability:"overgrow"},
+    {ID:"4", name: "Charmender", type: "fire", height: 2, ability:"blaze"},
+    {ID:"5", name: "Charmeleon", type: "fire", height: 3.58, ability:"blaze"},
+    {ID:"6", name: "Charizard", type: ["fire", "flying"], height: 5.58, ability:"blaze"},
+    {ID:"7", ame: "Squirtle", type: "water", height: 1.66, ability:"torrent"},
+    {ID:"8", ame: "Wartortle", type: "water", height: 3.25, ability:"torrent"},
+    {ID:"9", name: "Blastoise", type: "water", height: 5.25, ability:"torrent"},
+]
