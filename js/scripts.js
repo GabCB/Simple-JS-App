414415b60c13ff6caef4ce5b55a55f57023c29af
GabCB <gabycuencas@gmail.com>
2023-01-02 14:54:56 +0100

    Feat
    create 'for' loop that iterates over each item in pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,24 @@ let pokemonList = [
     {ID:"8", name: "Wartortle", type: "water", height: 1, ability:"torrent"},
     {ID:"9", name: "Blastoise", type: "water", height: 1.6, ability:"torrent"},
 ];
+
+/* loop to print Pokemon names list:
+for (let i = 0;
+    i < pokemonList.length;
+    i++) {
+        document.write(pokemonList[i].name + "<br>" )
+}*/
+
+for (let i = 0; 
+    i < pokemonList.length; 
+    i++) {
+    if (pokemonList[i].height >=5) {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m) - Wow, that is a big pokemon!" + "<br>")
+        }
+    else if (pokemonList[i].height >= 1.6 && pokemonList[i].height < 5) {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m) - That is a medium pokemon!" + "<br>")
+        }
+    else {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m) - That is a small pokemon!" + "<br>")
+        }
+}
