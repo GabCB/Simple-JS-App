b4d29eb3f5a2fd96951cd316a04886c7d86b02c5
GabCB <gabycuencas@gmail.com>
2023-01-06 12:01:40 +0100

    Updates scripts.js
    define new variable and wrap in IIFE to eliminate code from global use
    
    forEach loop uses pokemonRepository.getAll() instead of pokemonList to retrieve data now that array is protected by IIFE
    
    clean code

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,35 @@
-/*let pokemonList = [
-    {ID:"1", name: "Bulbasaur", type: ["grass", "poison"], height: 0.7, ability:"overgrow"},
-    {ID:"2", name: "Ivysaur", type: ["grass", "poison"], height: 1, ability:"overgrow"},
-    {ID:"3", name: "Venusaur", type: ["grass", "poison"], height: 2, ability:"overgrow"},
-    {ID:"4", name: "Charmender", type: "fire", height: 0.6, ability:"blaze"},
-    {ID:"5", name: "Charmeleon", type: "fire", height: 1.1, ability:"blaze"},
-    {ID:"6", name: "Charizard", type: ["fire", "flying"], height: 1.7, ability:"blaze"},
-    {ID:"7", name: "Squirtle", type: "water", height: 0.5, ability:"torrent"},
-    {ID:"8", name: "Wartortle", type: "water", height: 1, ability:"torrent"},
-    {ID:"9", name: "Blastoise", type: "water", height: 1.6, ability:"torrent"},
-];*/
-
-/* loop to print Pokemon names list:
-for (let i = 0;
-    i < pokemonList.length;
-    i++) {
-        document.write(pokemonList[i].name + "<br>" )
-}*/
-
-/*for (let i = 0; 
-    i < pokemonList.length; 
-    i++) {
-    if (pokemonList[i].height >=5) {
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m) - Wow, that is a big pokemon!" + "<br>")
-        }
-    else if (pokemonList[i].height >= 1.6 && pokemonList[i].height < 5) {
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m) - That is a medium pokemon!" + "<br>")
-        }
-    else {
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m) - That is a small pokemon!" + "<br>")
-        }
-}*/
-
-let pokemonRepository = (function () {
+/*define new variable and wrap in IIFE to eliminate code from global use */
+let pokemonRepository = (function() {
 
     let pokemonList = [
-        {ID:"1", name: "Bulbasaur", type: ["grass", "poison"], height: 0.7, ability:"overgrow"},
-        {ID:"2", name: "Ivysaur", type: ["grass", "poison"], height: 1, ability:"overgrow"},
-        {ID:"3", name: "Venusaur", type: ["grass", "poison"], height: 2, ability:"overgrow"},
-        {ID:"4", name: "Charmender", type: "fire", height: 0.6, ability:"blaze"},
-        {ID:"5", name: "Charmeleon", type: "fire", height: 1.1, ability:"blaze"},
-        {ID:"6", name: "Charizard", type: ["fire", "flying"], height: 1.7, ability:"blaze"},
-        {ID:"7", name: "Squirtle", type: "water", height: 0.5, ability:"torrent"},
-        {ID:"8", name: "Wartortle", type: "water", height: 1, ability:"torrent"},
-        {ID:"9", name: "Blastoise", type: "water", height: 1.6, ability:"torrent"},
+      { ID: "1", name: "Bulbasaur", type: ["grass", "poison"], height: 0.7, ability: "overgrow" },
+      { ID: "2", name: "Ivysaur", type: ["grass", "poison"], height: 1, ability: "overgrow" },
+      { ID: "3", name: "Venusaur", type: ["grass", "poison"], height: 2, ability: "overgrow" },
+      { ID: "4", name: "Charmender", type: "fire", height: 0.6, ability: "blaze" },
+      { ID: "5", name: "Charmeleon", type: "fire", height: 1.1, ability: "blaze" },
+      { ID: "6", name: "Charizard", type: ["fire", "flying"], height: 1.7, ability: "blaze" },
+      { ID: "7", name: "Squirtle", type: "water", height: 0.5, ability: "torrent" },
+      { ID: "8", name: "Wartortle", type: "water", height: 1, ability: "torrent" },
+      { ID: "9", name: "Blastoise", type: "water", height: 1.6, ability: "torrent" },
     ]
-
-    
-    function add (pokemon){
-        pokemonList.push(pokemon);
+  
+    function add(pokemon){
+      pokemonList.push(pokemon);
     }
-
-    function getAll () {
-        return pokemonList;
+  
+    function getAll() {
+      return pokemonList;
     }
-
+  
     return {
-        add: add,
-        getAll: getAll,
-    
+      add: add,
+      getAll: getAll
     };
+  
+  }) ();
+  
 
-}) ();
-
-document.write(pokemonRepository.getAll())
-
-pokemonRepository.getAll().forEach(function () {
-    pokemonRepository.add(pokemon);
-});
-
-/*replace for loop for forEach loop*/
+/*replace for loop for forEach loop
 pokemonList.forEach (function(pokemon) {
     if (pokemon.height >=5) {
         document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow, that is a big pokemon!" + "<br>")
@@ -78,4 +39,16 @@ pokemonList.forEach (function(pokemon) {
         document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is a small pokemon!" + "<br>")
     }
 //      console.log(pokemon);
-    });
+    });*/
+
+/* forEach loop uses pokemonRepository.getAll() instead of pokemonList to retrieve data now that array is protected by IIFE*/
+pokemonRepository.getAll().forEach(function(pokemon) {
+    if (pokemon.height >=5) {
+        document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow, that is a big pokemon!" + "<br>")
+    } else if (pokemon.height >= 1.6 && pokemon.height < 5) {
+          document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is a medium pokemon!" + "<br>")
+    } else {
+          document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is a small pokemon!" + "<br>")
+    }
+        
+});
